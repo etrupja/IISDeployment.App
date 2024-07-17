94f27310798582a2450e4119de717bb75ceda576
Ervis Trupja <ervistrupja@outlook.com>
2024-07-17 23:15:29 +0200

    Add project files.

---
+++ IISDeployment.App/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IISDeployment.App.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
